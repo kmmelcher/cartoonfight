9624d093c8229ce12fe2cc6ec0b6907a3fba6bfd
Kilian Melcher <kilian.melcher@gmail.com>
2020-11-09 12:14:38 -0300

    Add setup.py classifiers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cartoonfight", 
-    version="1.0.dev1",
+    version="1.0.dev3",
     author="Kilian Melcher",
     author_email="kilian.melcher@gmail.com",
     description="2D Fighting game with cartoon characters",
@@ -15,9 +15,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=['pygame>=2.0'],
     classifiers=[
+        "Development Status :: 1 - Planning",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Unix",
     ],
     python_requires='>=3.6',
 )
