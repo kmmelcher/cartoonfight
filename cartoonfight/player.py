bb0662d00a6620814d02ec94d989c9a779e9b9e0
Kilian Melcher <kilian.melcher@gmail.com>
2020-11-14 14:51:50 -0300

    Add two players movement

--- cartoonfight/player.py
+++ cartoonfight/player.py
@@ -4,44 +4,57 @@ import pygame
 #definition of a player class
 class Player(object):
 
-    def __init__(self, position, maxhealth, size, pOne, display):
+    def __init__(self, position, maxHealth, size, isPlayerOne, display):
         self.display = display
         self.position = position
         self.size = size
 
-        self.maxHealth = maxhealth
+        self.maxHealth = maxHealth
         self.health = self.maxHealth
         self.alive = True
         
-        self.standing = False
-        self.right = True
+        self.isPlayerOne = isPlayerOne
+
+        if self.isPlayerOne:
+            self.standingRight = True
+            self.standingLeft = False
+        else:
+            self.standingLeft = True
+            self.standingRight = False
+
+        self.right = False
         self.left = False
+
+        self.speed = 18
         
         self.framecount = 0
         
-        self.playerOne = pOne
+        
 
 
     def draw(self, sprites):
 
-        if self.standing:
-            #put here the animation of the standing caracter
+        if self.standingRight:
+            self.display.blit(sprites['WR0'], self.position)
+
+        elif self.standingLeft:
+            self.display.blit(sprites['WL0'], self.position)
+
+        elif self.right:
+            self.display.blit(sprites['WR'+str(1+self.framecount%4)], self.position)
+            self.framecount += 1
+
+        elif self.left:
+            self.display.blit(sprites['WL'+str(1+self.framecount%4)], self.position)
+            self.framecount += 1
+                
+        if self.isPlayerOne:
+		    #put here the left health bar
             pass
         else:
-            if self.right:
-                self.display.blit(sprites['WR'+str(self.framecount%4)], self.position)
-                #put here the animation of right walking caracter
-                pass
-            elif self.left:
-                #put here the animation of left walking caracter
-                pass
-                
-        if self.playerOne:
-		#put here the left health bar
-            pass
-        else:
-		#put here the right health bar
+
+		    #put here the right health bar
             pass
                 
-        self.framecount += 1
+        
 	
--- cartoonfight/start.py
+++ cartoonfight/start.py
@@ -2,11 +2,24 @@ import pygame
 
 from . import tools,player
 
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    K_a,
+    K_d,
+    KEYDOWN,
+    QUIT,
+)
+
 
 pygame.init()
 
+#Frames Per Second
 clock = pygame.time.Clock()
-FPS = 5
+FPS = 6
 
 #Game display
 monitor = pygame.display.Info()
@@ -25,8 +38,12 @@ IMAGES = tools.load_images_from_directories(_SUB_DIRECTORIES)
 #Scale background
 background = pygame.transform.scale(IMAGES['backgrounds']['default'], DISPLAY_SIZE)
 	
-playerOne = player.Player((10,316), 100, (128,128), True, DISPLAY)
-playerTwo = player.Player((700,380), 100, (64,64), False, DISPLAY)
+xOne = 10
+xTwo = DISPLAY_SIZE[0]-138
+FLOOR = DISPLAY_SIZE[1]-228
+
+playerOne = player.Player([xOne,FLOOR], 100, (128,128), True, DISPLAY)
+playerTwo = player.Player([xTwo,FLOOR+15], 100, (128,128), False, DISPLAY)
 
 def draw_window():
 	DISPLAY.blit(background,(0,0))
@@ -43,10 +60,53 @@ def game_loop():
 
 	while run:
 
-		#Quit Game
+		#User hit key
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
+			#Quit Game
+			if event.type == QUIT:
 				run = False
+
+		#User press key
+		user_press = pygame.key.get_pressed()
+		if user_press[K_d]:
+			playerOne.position[0] += playerOne.speed
+			playerOne.right = True
+			playerOne.left = False
+			playerOne.standingRight = False
+			playerOne.standingLeft = False
+		elif user_press[K_a]:
+			playerOne.position[0] -= playerOne.speed
+			playerOne.left = True
+			playerOne.right = False
+			playerOne.standingRight = False
+			playerOne.standingLeft = False
+		else:
+			if playerOne.right:
+				playerOne.standingRight = True
+			elif playerOne.left:
+				playerOne.standingLeft = True
+
+
+		if user_press[K_RIGHT]:
+			playerTwo.position[0] += playerTwo.speed
+			playerTwo.right = True
+			playerTwo.left = False
+			playerTwo.standingRight = False
+			playerTwo.standingLeft = False
+		elif user_press[K_LEFT]:
+			playerTwo.position[0] -= playerTwo.speed
+			playerTwo.left = True
+			playerTwo.right = False
+			playerTwo.standingRight = False
+			playerTwo.standingLeft = False
+		else:
+			if playerTwo.right:
+				playerTwo.standingRight = True
+			elif playerTwo.left:
+				playerTwo.standingLeft = True
+
+			playerTwo.right = False
+			playerTwo.left = False
 
 		draw_window()
 		clock.tick(FPS)
